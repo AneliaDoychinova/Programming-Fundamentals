112ca647dc4209e1ab42c54e99ef7ef8531a8a0c
AneliaDoychinova <aneliya_doychinova@abv.bg>
2017-05-26 15:24:33 +0300

    Added 9

---
+++ 02. C# Conditional Statements and Loops/ExercisesConditionalStatement/09. Count the Integers/09. Count the Integers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Count_the_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var count = 0;
+            try
+            {
+                var input = int.Parse(Console.ReadLine());
+                
+                while (true)
+                {
+                    count++;
+                    input = int.Parse(Console.ReadLine());
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(count);
+            }
+            
+            
+            
+        }
+    }
+}
