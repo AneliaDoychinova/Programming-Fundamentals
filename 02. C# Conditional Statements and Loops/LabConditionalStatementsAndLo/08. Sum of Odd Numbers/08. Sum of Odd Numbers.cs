91bd5a6622ca392dd7bb0da9fba0a1559c0f9190
AneliaDoychinova <aneliya_doychinova@abv.bg>
2017-05-26 15:31:22 +0300

    Added 8,9,10,11,12

---
+++ 02. C# Conditional Statements and Loops/LabConditionalStatementsAndLo/08. Sum of Odd Numbers/08. Sum of Odd Numbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            var sum = 0;
+            var count = 0;
+
+            for (int i = 1; ; i++)
+            {
+                if (count == number)
+                {
+                    break;
+                }
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                    count++;
+                }
+            }
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
---
+++ 02. C# Conditional Statements and Loops/LabConditionalStatementsAndLo/10. Multiplication Table 2.0/10. Multiplication Table 2.0.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier > 10)
+            {
+                var product = number * multiplier;
+                Console.WriteLine("{0} X {1} = {2}", number,multiplier,product);
+            }
+            for (int i = multiplier; i <= 10; i++)
+            {
+                var product = number * i;
+                Console.WriteLine("{0} X {1} = {2}", number, i, product);
+            }
+        }
+    }
+}
